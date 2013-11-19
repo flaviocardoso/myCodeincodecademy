efcb387045ccea7f8480d7a632c29f1c15a84c3f
Flavio Cardoso <flavioc41@gmail.com>
2013-11-19 01:15:04 -0200

    Update animalselfname.py

--- python_files/animalselfname.py
+++ python_files/animalselfname.py
@@ -1,7 +1,21 @@
+# class definition
 class Animal(object):
-    def __init__(self, name):
+    """Makes cute animals."""
+    # For initializing our instance objects
+    is_alive = True
+    def __init__(self, name, age, is_hungry):
         self.name = name
+        self.age = age
+        self.is_hungry = is_hungry
+        
+# Note that self is only used in the __init__()
+# function definition; we don't need to pass it
+# to our instance objects.
 
-zebra = Animal("Jeffrey")
+zebra = Animal("Jeffrey", 2, True)
+giraffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
 
-print(zebra.name)
+print(zebra.name, zebra.age, zebra.is_hungry)
+print(giraffe.name, giraffe.age, giraffe.is_hungry)
+print(panda.name, panda.age, panda.is_hungry)
