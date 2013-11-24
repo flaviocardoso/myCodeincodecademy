accdbd321db1dadadebdd9e79de95eaa9f1a399d
Flavio Cardoso <flavioc41@gmail.com>
2013-11-24 07:09:26 -0200

    Update files

---
+++ Python/readfile.py
@@ -0,0 +1,4 @@
+""" Read my file - output.txt""" 
+my_file = open("output.txt", "r")
+print my_file.read()
+my_file.close()
---
+++ Python/readfineline.py
@@ -0,0 +1,4 @@
+""" Read file 'text.txt' line by line """
+f = open("text.txt", "r")
+print f.readline()
+f.close()
